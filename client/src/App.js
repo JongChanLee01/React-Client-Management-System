b3a3da9c4e10e31e43344c6c6e8cd5283cce6fb0
JongChanLee <76044533+JongChanLee01@users.noreply.github.com>
2022-04-05 17:09:54 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -24,35 +24,62 @@ const styles = (theme) => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "이종찬",
-    birthday: "960000",
-    gender: "남자",
-    job: "취준생",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "김영진",
-    birthday: "950000",
-    gender: "남자",
-    job: "맘스터치 지점장",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "공태현",
-    birthday: "950000",
-    gender: "남자",
-    job: "프로그래머",
-  },
-];
+// 아래를 지우는 이유는 서버에서 데이터를 받아와서 출력을 할 것이기 때문에 더이상 필요가 없음
+
+// const customers = [
+//   {
+//     id: 1,
+//     image: "https://placeimg.com/64/64/1",
+//     name: "이종찬",
+//     birthday: "960000",
+//     gender: "남자",
+//     job: "취준생",
+//   },
+//   {
+//     id: 2,
+//     image: "https://placeimg.com/64/64/2",
+//     name: "김영진",
+//     birthday: "950000",
+//     gender: "남자",
+//     job: "맘스터치 지점장",
+//   },
+//   {
+//     id: 3,
+//     image: "https://placeimg.com/64/64/3",
+//     name: "공태현",
+//     birthday: "950000",
+//     gender: "남자",
+//     job: "프로그래머",
+//   },
+// ];
 
 class App extends Component {
+  // state는 변경될 수 있는 데이터를 처리할 때 사용
+  state = {
+    customers: "",
+  }
+
+  // api서버에 접근을 해서 데이터를 받아오는 등의 작업은 componentDidMount에서 해줄 수 있음
+  // 모든 component가 실제로 마운트가 완료가 되었을때 작동
+  componentDidMount() {
+    // 고객 데이터를 받아오고
+    this.callApi()
+      // 그것을 받아서 setState의 customers 변수에 넣음
+      .then((res) => this.setState({ customers: res }))
+      // 오류 문구
+      .catch((err) => console.log(err));
+  }
+
+  // 비동기적으로 수행할 수 있도록 해주는 함수 부분
+  callApi = async () => {
+    // 아래 경로로 접속을 해서 그 데이터들을 body에 저장을 함
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
   render() {
+    // props는 변경될 수 없는 그런 데이터를 명시 할때 사용
     const { classes } = this.props;
     return (
       <Paper className={classes.root}>
@@ -97,19 +124,23 @@ class App extends Component {
               // 위에 처럼 코드를 작성 할 경우 데이터가 늘어남에 따라 용량이 커지기 떄문에 반복함수를 사용하여 아래처럼 표현함.
               // JavaScript에서는 map이라는 함수로 반복시킬 수 있음.
               // map 함수를 사용할때에는 key 값이 있어야 함.
-              customers.map((c) => {
-                return (
-                  <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                );
-              })
+              // 위에서 state안에 값들을 비동기적으로 가져 왔기 때문에 처음에는 값이 비어져 있기 때문에 오류가 발생함
+              // 따라서 조건을 주어서 데이터 값이 존재 할 경우에만 출력 될 수 있도록 해줌
+              this.state.customers
+                ? this.state.customers.map((c) => {
+                    return (
+                      <Customer
+                        key={c.id}
+                        id={c.id}
+                        image={c.image}
+                        name={c.name}
+                        birthday={c.birthday}
+                        gender={c.gender}
+                        job={c.job}
+                      />
+                    );
+                  })
+                : ""
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -7,8 +7,37 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // api경로에 hello로 접속을 하면 아래 메시지를 보내도록 함
-app.get("/api/hello", (req, res) => {
-  res.send({ massage: "Hello Express!" });
+// app.get("/api/hello", (req, res) => {
+//   res.send({ massage: "Hello Express!" });
+// });
+
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "이종찬",
+      birthday: "960000",
+      gender: "남자",
+      job: "취준생",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "김영진",
+      birthday: "950000",
+      gender: "남자",
+      job: "맘스터치 지점장",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "공태현",
+      birthday: "950000",
+      gender: "남자",
+      job: "프로그래머",
+    },
+  ]);
 });
 
 // 서버가 동작중이면 동작중인 것을 알려줄 수 있도록 출력
